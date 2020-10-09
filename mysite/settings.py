c204ce2d1df18ed13c18ae2686132cc305b13843
Satoshi Kiriyama <kiriyama.sc@om.asahi-kasei.co.jp>
2020-10-09 09:08:15 +0900

    Secret Key move!!

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,15 +12,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from .setting_local import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%6t$i(98mu5^^6f(0&hoip*ktinz!#lzycz(^gk)eyu(#q=f6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
